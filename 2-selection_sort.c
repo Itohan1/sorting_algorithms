fed633593367f62ca084a0df7525088910ede5f5
Itohan1 <momoduitohan5@gmail.com>
2024-02-21 06:27:08 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int temp;
+
+	if (array == NULL)
+	{
+		return;
+	}
+
+	while (array && i < size - 1)
+	{
+		size_t Min = i;
+		size_t j = i + 1;
+
+		while (j < size)
+		{
+			if (array[j] < array[Min])
+			{
+				Min = j;
+			}
+			j++;
+		}
+		if (Min != i)
+		{
+			temp = array[i];
+			array[i] = array[Min];
+			array[Min] = temp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
